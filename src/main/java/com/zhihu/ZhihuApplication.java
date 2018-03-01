5cdaa9a00dcc599dcbeb4e97ff07d3351a315d59
w928028422 <928028422@qq.com>
2018-03-01 14:58:03 +0800

    Update ZhihuApplication.java

--- src/main/java/com/zhihu/ZhihuApplication.java
+++ src/main/java/com/zhihu/ZhihuApplication.java
@@ -2,10 +2,16 @@ package com.zhihu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.autoconfigure.solr.SolrAutoConfiguration;
 
 @SpringBootApplication(exclude = SolrAutoConfiguration.class)
-public class ZhihuApplication {
+public class ZhihuApplication extends SpringBootServletInitializer{
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ZhihuApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZhihuApplication.class, args);
